aa731ee7810dbe76ac4c4f9103b33d70e095e2f7
joezhou <joezhoux@foxmail.com>
2021-07-12 22:52:41 +0800

    updata

--- lodash/joezhoux-lodash.js
+++ lodash/joezhoux-lodash.js
@@ -1,12 +1,23 @@
 var joezhoux = {
   _identity: function (val, e) {
-    if (typeof(e) == "function") {
+    if (typeof (e) == "function") {
       return e(val);
-    } else if (typeof(e) == "string") {
+    } else if (typeof (e) == "string") {
       return val[e];
     }
   },
-  chunk: function (array, size=1) {
+  _arrayEach: function (array, iteratee) {
+    var index = -1,
+      length = array == null ? 0 : array.length;
+
+    while (++index < length) {
+      if (iteratee(array[index], index, array) === false) {
+        break;
+      }
+    }
+    return array;
+  },
+  chunk: function (array, size = 1) {
     if (size < 1) return [];
 
     let len = array == null ? 0 : array.length;
@@ -37,14 +48,15 @@ var joezhoux = {
     });
     return result;
   },
-  difference: function (array, diff) {
+  difference: function (array, ...diff) {
     if (!diff) return array;
+    let rules = diff.reduce((acc, curr) => acc.concat(curr))
 
     let len = array == null ? 0 : array.length;
     let result = [];
 
     for (let i = 0; i < len; i++) {
-      if (diff.indexOf(array[i]) == -1) {
+      if (rules.indexOf(array[i]) == -1) {
         result.push(array[i]);
       }
     }
@@ -83,5 +95,78 @@ var joezhoux = {
     }
     return ans;
   },
+  flattenDeep: function (array) {
+    let ans = [];
 
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] instanceof Array) {
+        ans.push(...this.flattenDeep(array[i]));
+      } else ans.push(array[i]);
+    }
+    return ans;
+  },
+  flattenDepth: function (array, depth = 1) {
+    let ans = [];
+    let d = depth;
+
+    for (let i = 0; i < array.length; i++) {
+      if (depth && array[i] instanceof Array) {
+        d--;
+        ans.push(...this.flattenDepth(array[i], d));
+      } else {
+        ans.push(array[i]);
+        d = depth;
+      }
+    }
+    return ans;
+  },
+  groupBy: function (array, fuc) {
+    let ans = {};
+
+    if (typeof fuc == "function") {
+      array.map(el => {
+        if (!ans.hasOwnProperty(fuc(el))) {
+          ans[fuc(el)] = [];
+        }
+        ans[fuc(el)].push(el);
+      });
+    } else if (fuc == "length") {
+      array.map(el => {
+        if (!ans.hasOwnProperty(el.length)) {
+          ans[el.length] = [];
+        }
+        ans[el.length].push(el);
+      });
+    }
+    return ans;
+  },
+  keyBy: function (array, fuc) {
+    let ans = {};
+
+    if (typeof fuc == "function") {
+      array.map(el => {
+        if (!ans.hasOwnProperty(fuc(el))) {
+          ans[fuc(el)] = [];
+        }
+        ans[fuc(el)].push(el);
+      });
+    } else if (typeof fuc == "string") {
+      array.map(el => {
+        if (!ans.hasOwnProperty(el[fuc])) {
+          ans[el[fuc]] = [];
+        }
+        ans[el[fuc]].push(el);
+      });
+    }
+    for (k in ans) {
+      if (ans[k].length == 1) {
+        ans[k] = ans[k][0];
+      }
+    }
+    return ans;
+  },
+  forEach: function (array, fuc) {
+
+  },
 }
+
